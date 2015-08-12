36efceaac9f98afc0de4ead2a80da75d09a87c13
Alejandro D'Ambrosio <alejandro.dambrosio@fluxit.com.ar>
2015-08-12 00:20:35 -0300

    Código de prueba Angular + PHP

---
+++ src/app.js
@@ -0,0 +1,43 @@
+var BASE_URL = '../php/';
+var angularTodo = angular.module('lostsysApp', []);
+ 
+function mainController($scope, $http) {
+    $scope.names = [ ];
+ 
+    $http.get(BASE_URL+'model.php')
+            .success(function(data) {
+                    $scope.names = eval(data);
+                    console.log(data)
+                })
+            .error(function(data) {
+                    console.log('Error: ' + data);
+            });
+ 
+    $scope.addNom = function() {
+        $http.post(BASE_URL+'model.php', { op: 'append', nom: $scope.nom, telefon: $scope.telefon } )
+                .success(function(data) {
+                        $scope.names = eval(data);
+                        console.log(data)
+                    })
+                .error(function(data) {
+                        console.log('Error: ' + data);
+                });
+ 
+        $scope.nom="";
+        $scope.telefon="";
+        }
+ 
+    $scope.delNom = function( nom ) {
+        if ( confirm("Seguro?") ) {
+            $http.post(BASE_URL+'model.php', { op: 'delete', nom: nom } )
+                .success(function(data) {
+                        $scope.names = eval(data);
+                        console.log(data)
+                    })
+                .error(function(data) {
+                        console.log('Error: ' + data);
+                });
+            }
+        }
+ 
+    }
